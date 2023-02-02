04596dfa1d0c045c6243cba47f0a237e7df25403
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2023-02-02 14:10:14 +0530

    hero section

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // navbar responsive
 let menu = document.querySelector(".menu");
 let navLinks = document.querySelector(".navlinks");
-let hero= document.querySelector(".hero")
+let image= document.querySelector(".image")
+let info= document.querySelector(".info")
+let mediaIcons= document.querySelector(".media-icons")
 
 menu.addEventListener('click',function(){
     navLinks.classList.toggle("active")
-
+    image.classList.toggle("hero-blur")
+    info.classList.toggle("hero-blur")
+    mediaIcons.classList.toggle("hero-blur")
 })
 
 
