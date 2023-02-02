47d1bdb3ce18c5443f2c91ebe5ef4beb5e05ec48
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2023-02-02 12:20:45 +0530

    navlinks change

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // navbar responsive
 let menu = document.querySelector(".menu");
 let navLinks = document.querySelector(".navlinks");
+let hero= document.querySelector(".hero")
 
 menu.addEventListener('click',function(){
     navLinks.classList.toggle("active")
+
 })
 
 
